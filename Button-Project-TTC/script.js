03dbca0f1356992e07d3d8e44b1bee30ba14a8d8
ahicken23 <132944599+ahicken23@users.noreply.github.com>
2023-07-06 17:15:01 -0600

    Add files via upload

---
+++ Button-Project-TTC/script.js
@@ -0,0 +1,20 @@
+const clicks = document.getElementById("clicks");
+
+let counter = 0;
+function btnPressed() {
+  counter = counter + 1;
+  clicks.innerHTML = counter;
+}
+
+function changeColor() {
+  let red = document.getElementById("rangeRed").value;
+  let green = document.getElementById("rangeGreen").value;
+  let blue = document.getElementById("rangeBlue").value;
+  let color = "rgb(" + red + "," + green + "," + blue + ")";
+  document.body.style.backgroundColor = color;
+  document.getElementById("colorOutput").innerHTML = ": " + color;
+}
+
+document.getElementById("rangeRed").addEventListener("input", changeColor);
+document.getElementById("rangeGreen").addEventListener("input", changeColor);
+document.getElementById("rangeBlue").addEventListener("input", changeColor);
